28be9bdd701c5a82d86eac98c79de1cda2a93d6a
Mihai Andronache <amihaiemil@gmail.com>
2016-03-26 21:48:48 +0200

    script to select page based on query param

--- scripts/pages.js
+++ scripts/pages.js
@@ -1,65 +1,103 @@
 $(document).ready(function() {
+    var pageFromUrl = getPageFromUrl();
+    if(pageFromUrl == 'authors') {
+        goToAuthorsPage();
+    } else if (pageFromUrl == 'usage') {
+        goToUsagePage();
+    } else if(pageFromUrl == 'releases') {
+        goToReleasesPage();
+    }
     $("#homeButton").on("click", function() {
-        $("#authors").hide();
-        $("#releases").hide();
-        $("#usage").hide();
-        $("#intro").show();
-        $("#usageButton").removeClass("selected");
-        $("#releasesButton").removeClass("selected");
-        $("#authorsButton").removeClass("selected");
-
-        $(this).addClass("selected");
-        $("#homeButton img").attr("src","images/icons_light/home_selected.png");
-        $("#usageButton img").attr("src","images/icons_light/usage.png");
-        $("#releasesButton img").attr("src","images/icons_light/releases.png");
-        $("#authorsButton img").attr("src","images/icons_light/authors.png");
+        goToHomePage();
     });
 
     $("#usageButton").on("click", function() {
-        $("#authors").hide();
-        $("#releases").hide();
-        $("#intro").hide();
-        $("#usage").show();
-        $("#homeButton").removeClass("selected");
-        $("#releasesButton").removeClass("selected");
-        $("#authorsButton").removeClass("selected");
-
-        $(this).addClass("selected");
-        $("#homeButton img").attr("src","images/icons_light/home.png");
-        $("#usageButton img").attr("src","images/icons_light/usage_selected.png");
-        $("#releasesButton img").attr("src","images/icons_light/releases.png");
-        $("#authorsButton img").attr("src","images/icons_light/authors.png");
+        goToUsagePage();
     });
 
     $("#releasesButton").on("click", function() {
-        $("#authors").hide();
-        $("#intro").hide();
-        $("#usage").hide();
-        $("#releases").show();
-        $("#usageButton").removeClass("selected");
-        $("#homeButton").removeClass("selected");
-        $("#authorsButton").removeClass("selected");
-
-        $(this).addClass("selected");
-        $("#homeButton img").attr("src","images/icons_light/home.png");
-        $("#usageButton img").attr("src","images/icons_light/usage.png");
-        $("#releasesButton img").attr("src","images/icons_light/releases_selected.png");
-        $("#authorsButton img").attr("src","images/icons_light/authors.png");
+        goToReleasesPage();
     });
 
     $("#authorsButton").on("click", function() {
-        $("#intro").hide();
-        $("#releases").hide();
-        $("#usage").hide();
-        $("#authors").show();
-        $("#usageButton").removeClass("selected");
-        $("#releasesButton").removeClass("selected");
-        $("#homeButton").removeClass("selected");
-
-        $(this).addClass("selected");
-        $("#homeButton img").attr("src","images/icons_light/home.png");
-        $("#usageButton img").attr("src","images/icons_light/usage.png");
-        $("#releasesButton img").attr("src","images/icons_light/releases.png");
-        $("#authorsButton img").attr("src","images/icons_light/authors_selected.png");
+        goToAuthorsPage();
     });
 });
+
+function getPageFromUrl()
+{
+    var paramsMap = [], param;
+    var params = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < params.length; i++)
+    {
+        param = params[i].split('=');
+        if(param[0]=='page') {
+            return param[1];
+        }
+    }
+    return 'intro';
+}
+
+function goToHomePage() {
+    $("#authors").hide();
+    $("#releases").hide();
+    $("#usage").hide();
+    $("#intro").show();
+    $("#usageButton").removeClass("selected");
+    $("#releasesButton").removeClass("selected");
+    $("#authorsButton").removeClass("selected");
+
+    $("#homeButton").addClass("selected");
+    $("#homeButton img").attr("src","images/icons_light/home_selected.png");
+    $("#usageButton img").attr("src","images/icons_light/usage.png");
+    $("#releasesButton img").attr("src","images/icons_light/releases.png");
+    $("#authorsButton img").attr("src","images/icons_light/authors.png");
+}
+
+function goToAuthorsPage() {
+    $("#intro").hide();
+    $("#releases").hide();
+    $("#usage").hide();
+    $("#authors").show();
+    $("#usageButton").removeClass("selected");
+    $("#releasesButton").removeClass("selected");
+    $("#homeButton").removeClass("selected");
+
+    $("#authorsButton").addClass("selected");
+    $("#homeButton img").attr("src","images/icons_light/home.png");
+    $("#usageButton img").attr("src","images/icons_light/usage.png");
+    $("#releasesButton img").attr("src","images/icons_light/releases.png");
+    $("#authorsButton img").attr("src","images/icons_light/authors_selected.png");
+}
+
+function goToUsagePage() {
+    $("#authors").hide();
+    $("#releases").hide();
+    $("#intro").hide();
+    $("#usage").show();
+    $("#homeButton").removeClass("selected");
+    $("#releasesButton").removeClass("selected");
+    $("#authorsButton").removeClass("selected");
+
+    $("#usageButton").addClass("selected");
+    $("#homeButton img").attr("src","images/icons_light/home.png");
+    $("#usageButton img").attr("src","images/icons_light/usage_selected.png");
+    $("#releasesButton img").attr("src","images/icons_light/releases.png");
+    $("#authorsButton img").attr("src","images/icons_light/authors.png");
+}
+
+function goToReleasesPage() {
+    $("#authors").hide();
+    $("#intro").hide();
+    $("#usage").hide();
+    $("#releases").show();
+    $("#usageButton").removeClass("selected");
+    $("#homeButton").removeClass("selected");
+    $("#authorsButton").removeClass("selected");
+
+    $("#releasesButton").addClass("selected");
+    $("#homeButton img").attr("src","images/icons_light/home.png");
+    $("#usageButton img").attr("src","images/icons_light/usage.png");
+    $("#releasesButton img").attr("src","images/icons_light/releases_selected.png");
+    $("#authorsButton img").attr("src","images/icons_light/authors.png");
+}
